5d766d8d318d5cf32988041542c714a1e5214cba
Xavier Pereta <xpereta@gmail.com>
2025-05-06 16:34:59 +0200

    Add Xcode project

---
+++ Mortyverse/MortyverseApp.swift
@@ -0,0 +1,20 @@
+//
+//  MortyverseApp.swift
+//  Mortyverse
+//
+//  Created by Xavier Pereta París on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MortyverseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
