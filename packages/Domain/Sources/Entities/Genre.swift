a1cd267eec219d27d05697a03824b48a8ea87f5c
Xavier Pereta <xpereta@gmail.com>
2025-06-15 17:01:56 +0200

    Remove unnided custom Equatable implementation

--- packages/Domain/Sources/Entities/Genre.swift
+++ packages/Domain/Sources/Entities/Genre.swift
@@ -1,18 +1,11 @@
 import Foundation
 
-public struct Genre: Identifiable {
+public struct Genre: Identifiable, Equatable {
     public let id: Int
     public let name: String
 
     public init(id: Int, name: String) {
         self.id = id
         self.name = name
-    }
-}
-
-extension Genre: Equatable {
-    // Optimized equatable implementation, id is enough for our use cases
-    public static func == (lhs: Genre, rhs: Genre) -> Bool {
-        return lhs.id == rhs.id
     }
 }
--- packages/Domain/Tests/Entities/GenreTests.swift
+++ packages/Domain/Tests/Entities/GenreTests.swift
@@ -10,16 +10,4 @@ final class GenreTests {
         #expect(genre.id == 1)
         #expect(genre.name == "Action")
     }
-
-    @Test("Genre equality comparison")
-    func testGenreEquality() {
-        let genre1 = Genre(id: 1, name: "Action")
-        let genre2 = Genre(id: 1, name: "Drama") // Same ID, but different name
-        let genre3 = Genre(id: 2, name: "Action") // Different ID, but same name
-
-        // Genres with same ID should be equal regardless of name
-        #expect(genre1 == genre2)
-        // Genres with different IDs should not be equal
-        #expect(genre1 != genre3)
-    }
 }
